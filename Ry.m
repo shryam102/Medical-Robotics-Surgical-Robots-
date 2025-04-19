00bc71f1e88b161e7af383b5e3e79ccb7abc3fc1
Shresth Verma <78613519+shryam102@users.noreply.github.com>
2025-04-19 02:52:01 -0400

    Add files via upload

---
+++ Ry.m
@@ -0,0 +1,5 @@
+function x = Ry(theta)
+
+x = [cos(theta) 0 sin(theta); 
+     0          1     0     ; 
+    -sin(theta) 0 cos(theta)];
---
+++ Rz.m
@@ -0,0 +1,5 @@
+function x = Rz(phi)
+
+x = [cos(phi) -sin(phi) 0; 
+     sin(phi)  cos(phi) 0; 
+     0           0      1];
